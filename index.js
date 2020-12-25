80cc9b3334030dde8ed08be83f2a7269df0b1ab1
EKANT KHUSHAL KAPGATE <2016bcs105@sggs.ac.in>
2020-12-25 18:15:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
     var position = $("#myCanvas").offset();
-    $("#box3").css({
-        top: +position.top + 200 - 20,
+
+    $("#box4").css({
+        top: +position.top + 200 - 11,
         left: +position.left
     });
+    $("#box3").css({
+        top: +position.top + 200 - 21,
+        left: +position.left+10
+    });
     $("#box2").css({
-        top: +position.top + 200 - 40,
-        left: +position.left + 10
+        top: +position.top + 200 - 31,
+        left: +position.left + 20
     });
     $("#box1").css({
-        top: +position.top + 200 - 60,
-        left: +position.left + 20
+        top: +position.top + 200 - 41,
+        left: +position.left + 30
     });
 
 });
@@ -20,11 +25,8 @@ var dict = {};
 dict['A'] = 0;
 dict['B'] = 1;
 dict['C'] = 2;
-var state = [
-    [1, 2, 3],
-    [],
-    []
-]
+var state =
+ [    [1, 2, 3,4], [], []]
 
 var steps = new Array();
 
@@ -53,7 +55,10 @@ function towerOfHanoi(num, initial, target, auxilary) {
     if (num == 1) {
         steps.push(function() {
             $.when(move(num, initial, target)).done(function() {
-                console.log(initial + " " + target);
+                console.log("move disk 1 from tower " +initial +" to tower " +target);
+                var p = document.createElement("p");
+                p.innerHTML="move disk 1 from tower " +initial +" to tower " +target;
+                document.getElementById("log").appendChild(p);
             });
         });
         return;
@@ -61,7 +66,10 @@ function towerOfHanoi(num, initial, target, auxilary) {
     towerOfHanoi(num - 1, initial, auxilary, target);
     steps.push(function() {
         $.when(move(num, initial, target)).done(function() {
-            console.log(num + " from tower " + initial + " to tower " + target);
+            console.log("move disk "+ num + " from tower " + initial + " to tower " + target);
+            var p = document.createElement("p");
+            p.innerHTML="move disk 1 from tower " +initial +" to tower " +target;
+            document.getElementById("log").appendChild(p);
         });
     });
 
@@ -72,16 +80,15 @@ function move(num, start, end) {
     var arr2 = status(start, end, num);
     var id = "#box" + num;
     var len = arr2.length - 1;
-    console.log(len);
     var x = $(id);
     x.animate({
-        top: '20px'
+        top: '80px'
     }, "slow", function() {
         x.animate({
             left: (x.offset().left + (dict[end] - dict[start]) * 150)
         }, "slow", function() {
             x.animate({
-                top: (200 - len * 20)
+                top: (257 - len * 10)
             }, "slow", function() {
 
             });
@@ -91,7 +98,7 @@ function move(num, start, end) {
 $(document).ready(function() {
     function solve() {
 
-        towerOfHanoi(3, 'A', 'B', 'C');
+        towerOfHanoi(4, 'A', 'B', 'C');
 
         for (var i = 0, len = steps.length; i < len; i++) {
             var fn = steps[i];
@@ -104,7 +111,8 @@ $(document).ready(function() {
     }
 
     $("#btn1").click(function() {
-
         solve();
     });
 });
+
+
